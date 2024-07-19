6513e5e45b0bb9d8f9ecdb192aab0f1978c3b62d
Alexander <alexanderkudryashov01@gmail.com>
2024-07-19 18:59:12 +0300

    fixing

--- creating_db.sql
+++ creating_db.sql
@@ -8,3 +8,41 @@ CREATE DATABASE TaskDB;  -- CREATE DATABASE - создание базы данн
 
 -- Подключаемся к базе данных TaskDB (для командной оболочки psql)
 -- \c TaskDB  -- подключение к базе данных TaskDB
+
+-- Создаем таблицу Operators
+CREATE TABLE Operators (
+    OperatorID SERIAL PRIMARY KEY,  -- SERIAL - автоинкрементный тип данных, PRIMARY KEY - первичный ключ
+    Name VARCHAR(100),  -- VARCHAR - строковый тип данных с максимальной длиной 100 символов
+    StartDate DATE  -- DATE - тип данных для хранения даты
+);
+
+-- Создаем таблицу Debtors
+CREATE TABLE Debtors (
+    DebtorID SERIAL PRIMARY KEY,  -- SERIAL - автоинкрементный тип данных, PRIMARY KEY - первичный ключ
+    Name VARCHAR(100),  -- VARCHAR - строковый тип данных с максимальной длиной 100 символов
+    DebtAmount DECIMAL(10, 2)  -- DECIMAL - числовой тип данных с точностью, (10, 2) - 10 знаков до и 2 после запятой
+);
+
+-- Создаем таблицу Phones
+CREATE TABLE Phones (
+    PhoneID SERIAL PRIMARY KEY,  -- SERIAL - автоинкрементный тип данных, PRIMARY KEY - первичный ключ
+    DebtorID INT REFERENCES Debtors(DebtorID),  -- INT - целочисленный тип данных, REFERENCES - создание внешнего ключа
+    PhoneNumber VARCHAR(15)  -- VARCHAR - строковый тип данных с максимальной длиной 15 символов
+);
+
+-- Создаем таблицу Payments
+CREATE TABLE Payments (
+    PaymentID SERIAL PRIMARY KEY,  -- SERIAL - автоинкрементный тип данных, PRIMARY KEY - первичный ключ
+    DebtorID INT REFERENCES Debtors(DebtorID),  -- INT - целочисленный тип данных, REFERENCES - создание внешнего ключа
+    PaymentDate DATE,  -- DATE - тип данных для хранения даты
+    PaymentAmount DECIMAL(10, 2)  -- DECIMAL - числовой тип данных с точностью, (10, 2) - 10 знаков до и 2 после запятой
+);
+
+-- Создаем таблицу Addresses
+CREATE TABLE Addresses (
+    AddressID SERIAL PRIMARY KEY,  -- SERIAL - автоинкрементный тип данных, PRIMARY KEY - первичный ключ
+    DebtorID INT REFERENCES Debtors(DebtorID),  -- INT - целочисленный тип данных, REFERENCES - создание внешнего ключа
+    Address VARCHAR(255),  -- VARCHAR - строковый тип данных с максимальной длиной 255 символов
+    City VARCHAR(100),  -- VARCHAR - строковый тип данных с максимальной длиной 100 символов
+    PostalCode VARCHAR(10)  -- VARCHAR - строковый тип данных с максимальной длиной 10 символов
+);
